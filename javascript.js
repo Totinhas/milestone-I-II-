abcda6bf83442614ff79ead4c2ca71d1f4ade9f4
Sara Bretes <SBX14@charles-stanley.co.uk>
2020-01-27 15:27:26 +0000

    updates on the shopping cart js

--- javascript.js
+++ javascript.js
@@ -8,7 +8,12 @@ function remove() {}
 function add() {
   for (let i = 0; i < price.length; i++) {
     console.log(price[i].innerText);
-  }
+    
+    //only show the remove button if there are items of that type on the cart
+    //add.addEventListener ("click", function () {
+     // removebtn.classList.toggle("display-none");
+    //})
+  } 
 }
 function total() {
   for (let q = 0; q < quantity.length; q++) {
@@ -16,10 +21,19 @@ function total() {
   }
 }
 
+
+
 function bla() {
   let total = document.getElementById("total-value");
 
   if (quantity > 0) {
     total = quantity * 2;
   }
+}
+
+
+//Youtube 1
+
+function () {
+  let 
 }
